35202d741b33e69243db679ec3d04bea5a6c4e65
Jeff Russell <jeff.russell@alight.com>
2024-05-22 13:26:52 -0500

    testing failure for trace

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,10 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('Should Fail', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
