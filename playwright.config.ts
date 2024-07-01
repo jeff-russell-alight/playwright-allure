848a7c0772175794ff3223b252917e8c5533510a
Jeff Russell <jeff.russell@alight.com>
2024-07-01 11:11:59 -0500

    adding allure back in

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   grep: testPlanFilter(),
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [currentsReporter(currentsConfig)] : [['line'], ["allure-playwright"]],
+  reporter: process.env.CI ? [["allure-playwright"]] : [['line']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
